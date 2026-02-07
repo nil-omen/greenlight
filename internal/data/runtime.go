de642afcb13afc7298965233f3e241632b1db22a
King <24613526+nil-omen@users.noreply.github.com>
2026-02-08 00:00:35 +0200

    Add Runtime type with JSON marshal/unmarshal
    MarshalJSON emits values as quoted strings like "120 mins".
    UnmarshalJSON parses the same format and returns
    ErrInvalidRuntimeFormat for malformed input.
    Implemented the json.Unmarshaler interface

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,14 +1,39 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unqoutedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unqoutedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
