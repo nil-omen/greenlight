4e63a884b9eaad98003aa7d2d6e1da9b4641e694
King <24613526+nil-omen@users.noreply.github.com>
2026-02-20 19:01:21 +0200

    feat: Implement email sending functionality to send welcome emails to new users.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 
 	_ "github.com/lib/pq"
 	"github.com/nil-omen/greenlight/internal/data"
+	"github.com/nil-omen/greenlight/internal/mailer"
 )
 
 const version = "0.1.0"
@@ -28,12 +29,20 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *slog.Logger
 	models data.Models
+	mailer *mailer.Mailer
 }
 
 func main() {
@@ -50,6 +59,12 @@ func main() {
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP Host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 587, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "bf5c745d66a379", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "738869a52eec09", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <king@nil-omen.net>", "SMTP sender")
 
 	flag.Parse()
 
@@ -68,10 +83,17 @@ func main() {
 
 	logger.Info("database connection pool established")
 
+	mailer, err := mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
 	app := &application{
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer,
 	}
 
 	err = app.serve()
--- go.mod
+++ go.mod
@@ -10,4 +10,9 @@ require golang.org/x/time v0.14.0
 
 require github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce
 
-require golang.org/x/crypto v0.48.0
+require (
+	github.com/wneessen/go-mail v0.7.2
+	golang.org/x/crypto v0.48.0
+)
+
+require golang.org/x/text v0.34.0 // indirect
