097df16b99bfac4c0330c7c88e392462cf2d6b81
King <24613526+nil-omen@users.noreply.github.com>
2026-03-06 13:57:41 +0200

    Feat: implemented authenticate() and createAuthenticationTokenHandler() methods and set the route, header and middleware. used new appropriate errors responses and created new anonymousUser variable.

---
+++ cmd/api/context.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/nil-omen/greenlight/internal/data"
+)
+
+type contextkey string
+
+const userContextKey = contextkey("user")
+
+func (app *application) contextSetUser(r *http.Request, user *data.User) *http.Request {
+	ctx := context.WithValue(r.Context(), userContextKey, user)
+	return r.WithContext(ctx)
+}
+
+func (app *application) contextGetUser(r *http.Request) *data.User {
+	user, ok := r.Context().Value(userContextKey).(*data.User)
+	if !ok {
+		panic("missing user value in request context")
+	}
+
+	return user
+}
